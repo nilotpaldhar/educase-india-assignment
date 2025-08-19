45881ae1ab48328f0f9663f7b8d0ebfa68124f7e
Nilotpal Dhar <nilotpaldhar1997@gmail.com>
2025-08-19 16:31:03 +0530

    chore: initial commit with project boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="max-w-2xs mx-auto py-5">
+      <h1 className="font-medium text-[28px]">Welcome to PopX</h1>
+      <p className="text-lg opacity-60">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+    </div>
+  );
+};
+
+export { App };
